bd18188a43f405498efda0d372bfad8b0896984b
Rinkky <vurvur1@hotmail.gr>
2018-10-11 21:34:30 +0300

    hanehoa

---
+++ README.md
@@ -0,0 +1,16 @@
+# BlockCraft
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<h2>Owner: Steven4547466#1407</h2>
+<h2>Blockcraft is a minecraft SMP 1.13 server inspired by Hermitcraft, </n>
+that consists of 21 players, all building with their own </n>
+unique style. This team is here to entertain you, teach you, </n>
+and probably make you wanna play minecraft even more!</h2>
+
+
+</body>
+</html>
---
+++ index.js
@@ -0,0 +1,249 @@
+﻿// Calling the package
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const fs = require('fs');
+const moment = require('moment') // the moment package. to make this work u need to run "npm install moment --save 
+const config = require("./config.json");
+const ms = require("ms") // npm install ms -s
+
+// json files
+
+// Listener Event: Bot Launched
+bot.on('ready', () => {
+    console.log('Power Level Stabilised.') // Runs when the bot is launched
+
+    //const botchat = bot.channels.get("469992574791319552")
+    //const generalchat = bot.channels.get("469490700845580298")
+    //generalchat.send(`Topic of the week: After the attack towards the coven from a demon, Steven demands all clans to be disbanded. The server has to choose: does he want the end of all clans? ***React to the announcement on #announcements reguarding the topic with a 👍 if you agree, or with a 👎 if you want clans to stay!***`)
+    
+    
+    bot.user.setActivity("prefix ` | Blocks Awakens")
+
+});
+
+//event listener: join/leave a voice channel
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
+  let ivc = newMember.guild.roles.find("name", "In Voice Call");
+  
+  if(oldUserChannel === undefined && newUserChannel !== undefined) { // User Joins a voice channel
+        newMember.addRole(ivc).catch(console.error);
+  } else if(newUserChannel === undefined) { // User leaves a voice channel
+    newMember.removeRole(ivc).catch(console.error);
+  }
+});
+
+
+// event listener: new guild members
+bot.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find('name', 'general');
+    const channelinfo = member.guild.channels.find('name', 'info');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome ${member} to BlockCraft SMP's discord! Make sure to check out the ${channelinfo} channel! `);
+    
+  });
+
+// Event listener: Message Received ( This will run every time a message is received)
+bot.on('message', async message => {
+
+    // Variables
+    let sender = message.author; // The person who sent the message
+    let msg = message.content.toLowerCase();
+    let prefix = '`' // The text before commands
+    if (bot.user.id === sender.id) { return }
+    let nick = sender.username
+    let Owner = message.guild.roles.find('name', "Owner")    
+
+
+    // commands
+
+    // Ping / Pong command
+    if (msg === prefix + 'ping') {
+      if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
+        let m = await message.channel.send("Ping?");
+        m.edit(`Pong. Latency: ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
+      } else {return}
+    }
+
+
+    //Pollbot replacement
+    if (msg.startsWith("poll:")) {
+      if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) { 
+            let m = await message.react("👍")
+            let m2 = await message.react("👎")
+            let m3 = await message.react("🤷")
+        } else {return};
+      };
+
+    //4poll
+    if (msg.startsWith("4poll:")) {
+      if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) { 
+            let m = await message.react("🤔")
+            let m2 = await message.react("👉")
+            let m3 = await message.react("👌")
+            let m4 = await message.react("🖕")
+        } else {return};
+      };
+
+
+    //timed message
+    const generalchat = bot.channels.get("469490700845580298")
+    let timer = bot.setInterval(timedMessage, /*172800000*/10800000);
+    //let timer2 = bot.setInterval(timedMessage2, 300000);
+    
+    //function timedMessage() {
+      //generalchat.send(`Topic of the week: `)
+      //.catch(console.error)};
+
+    /*activities = ["prefix ` | Investigating Mafia",
+                  "prefix ` | Searching For The coven",
+                  "prefix ` | Having A Meeting",
+                  "prefix ` | Talking With The Agency",
+                  "prefix ` | Building SkyBases",
+                  "prefix ` | ¯\_(ツ)_/¯"]
+    
+    function timedMessage2() {
+      var result = Math.floor((Math.random() * activities.length) + 0)
+      bot.user.setActivity((activities[result])).catch(console.error)
+    };*/
+
+
+    //bot info command
+    if (msg === prefix + "botinfo") {
+        let bicon = bot.user.displayAvatarURL
+
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor(0x15f153)
+        .setThumbnail(bicon)
+        .addField("Bot Name", bot.user.username)
+        .addField("Created At", bot.user.createdAt)
+
+        message.channel.send(botembed)
+    };
+
+
+    //serverinfo command
+    if (msg === prefix + "serverinfo") {
+      let sicon = message.guild.displayAvatarURL
+        
+        let serverembed = new Discord.RichEmbed()
+        .setDescription("__**Server Information**__")
+        .setColor(0x15f153)
+        .setThumbnail(sicon)
+        .addField("Server Name", message.guild.name)
+        .addField("Created On", message.guild.createdAt)
+        .addField("Total Members", message.guild.memberCount)
+        .addField("Emoji", bot.emojis + "*work in progress hehexd*")
+
+        await message.channel.send(serverembed)
+
+    };
+
+    //member info
+    if (msg.split(" ")[0] === prefix + "member") {
+      //ex `member @Rinkky
+      let args = msg.split(" ").slice(1)
+      let rMember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+      let micon = rMember.displayAvatarURL
+
+        if(!rMember) 
+          return message.reply("No user Specified")
+
+          let memberembed = new Discord.RichEmbed()
+          .setDescription("__**Member Information**__")
+          .setColor(0x15f153)
+          .setThumbnail(micon)
+          .addField("Name", rMember)
+          .addField("ID", rMember.id)
+          .addField("Joined at", rMember.joinedAt)
+  
+          await message.channel.send(memberembed)
+
+    };
+
+
+    //reports
+    if (msg.split(" ")[0] === prefix + "report") {
+      //ex `report @Rinkky racist
+      let args = msg.split(" ").slice(1)
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+      let rreason = args.join(" ").slice(22)
+      let reportschannel = message.guild.channels.find(`name`, "logs")
+
+        message.delete()
+
+        if(!rUser) return message.reply("There's no such user.")
+        if(!rreason) return message.reply("You can't report without a good reason.")
+
+        let reportEmbed = new Discord.RichEmbed()
+        .setDescription("Report-ing for duty!")
+        .setColor(0xe0782b)
+        .addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+        .addField("Reported By", `${sender} with ID: ${sender.id}`)
+        .addField("Reason", rreason)
+        .addField("Channel", message.channel)
+        .addField("Reported At", message.createdAt)
+        
+
+        reportschannel.send(reportEmbed)
+        message.guild.members.get(sender.id)
+        .createDM()
+              .then(dm => {
+                dm.send({embed: {
+                  color: 0x15f153,
+                  title: "User Reported" ,
+                 description: `You successfully reported ${rUser}. \nReason:${rreason} \n\n Thank you for your help, we'll investigate.`,
+                 timestamp: new Date(),
+                  footer: {
+                  icon_url: "186487324517859328".avatarURL,
+                  text: "Any intentionally misleading reports will not be tolorated"
+                  }
+                }})
+              })
+    };
+
+
+    //EVAL! DO NOT FUCKING TOUCH THAT SHIT IF YOU ARE NOT RINKKY!
+
+    if (msg.startsWith(prefix + "eval")) {
+      if(sender.id !== "186487324517859328") return;
+      const args = message.content.split(" ").slice(1);
+      try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+  
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+  
+        message.channel.send(clean(evaled), {code:"xl"});
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      }
+    }
+
+
+    //stopping the bot
+    if (msg === prefix + 'stop') {
+      if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
+        process.exit(1)
+      } else {return}
+    };
+
+}); //the end of bot.on ------------------------------
+
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+//  Login
+
+// the bot.token('token')
+bot.login(process.env.token);
