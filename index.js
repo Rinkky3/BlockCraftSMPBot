6e919611fb6a775f60cc3dc8b1c2fc9447e1c9a6
Rinkky <vurvur1@hotmail.gr>
2018-10-16 23:51:38 +0300

    first commit

--- index.js
+++ index.js
@@ -164,6 +164,28 @@ bot.on('message', async message => {
 
     };
 
+    //role info
+    if (msg.split(" ")[0] === prefix + "roleinfo") {
+      //ex `roleinfo @owner
+      let args = msg.split(" ").slice(1)
+      let rRole = message.guild.member(message.mentions.roles.first() || message.guild.roles.get(args[0]))
+      let rmembers = message.guild.roles.get(role.id).members.map(m => m.user.tag);
+      
+
+        if(!rRole) 
+          return message.reply("Who dat role? I cant find it.")
+
+          let memberembed = new Discord.RichEmbed()
+          .setDescription("__**Role Information**__")
+          .setColor(0x15f153)
+          .addField("Name", rRole)
+          .addField("ID", rRole.id)
+          .addField(`Members assigned to this role: ${message.guild.members.filter(m =>!m.user.bot).filter(m => m.roles.get(role.id)).map(m => `\n[${m.user.username} : ${m.user.id}]`)}`)
+  
+          await message.channel.send(memberembed)
+
+    };
+
 
     //reports
     if (msg.split(" ")[0] === prefix + "report") {
