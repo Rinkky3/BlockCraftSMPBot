c51baa2b15a0a147249ddd5fd3ec1cfe965c06d2
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2018-10-18 20:37:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const moment = require('moment') // the moment package. to make this work u need to run "npm install moment --save 
 const ms = require("ms") // npm install ms -s
 
-// Some stuff dw about it
+// Okay, i wont worry about it ;)
 const workCooldown = new Set();
 
 // json files
@@ -60,7 +60,8 @@ bot.on('message', async message => {
     if (bot.user.id === sender.id) { return }
     let nick = sender.username
     let Owner = message.guild.roles.find('name', "Owner")    
-
+    let Staff = message.guild.roles.find('name', "Staff")
+    
     //json stuff
     if (!userData[sender.id]) userData[sender.id] = {}
     if (!userData[sender.id].money) userData[sender.id].money = 0;
@@ -79,16 +80,50 @@ bot.on('message', async message => {
         let m = await message.channel.send("Ping?");
         m.edit(`Pong. Latency: ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
       } else {return}
-    }
-    
-    // leaderboard
-    if (msg === prefix + "lb" || msg === prefix + "leaderboard"){
-        let lb = new Discord.RichEmbed()
-        .setDescription('**___Leaderboard___**')
-        .setColor(0x15f153)
-        message.channel.send(lb)
     };
     
+    // Applications and stuff
+    if (msg === prefix + 'applied'){
+        let appchannel = message.guild.channels.find(`name`, "staff")
+        let pending = message.guild.roles.find('name', "In-Progress")    
+        if (!message.member.roles.has(pending.id)) return message.channel.send(sender + ", you are not in progress!")
+            let m = await message.reply('I have notified the staff that you have applied, please ensure that you\'re answers are at least a paragraph long, if they are not, your application will be discarded.')
+            
+            let applyEmbed = new Discord.RichEmbed()
+            .setDescription("**___New application___**")
+            .setColor(0x15f153)
+            .addField('Name:', sender)
+            
+            appchannel.send(applyEmbed)
+    };
+ 
+    // Deny
+    if (msg.split(" ")[0] === prefix + "deny"){
+      message.delete()
+      let args = msg.split(" ").slice(1)
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+      let rreason = args.join(" ").slice(22)
+      if (!message.member.roles.has(Owner.id) && !message.member.roles.has(Staff.id)) return message.channel.send("You do not have access to this command")
+      if (!rUser) return message.channel.send('This user doesn\'t exist')
+      let denyEmbed = new Discord.RichEmbed()
+      .setDescription("**___User Denied___**")
+      .setColor(0xFF0000)
+      .addField('Name of user denied:', rUser)
+      .addField('Reason', rreason)
+      .addField('Retry', "You are good to retry as long as you haven't been denied multiple times. Just apply again!")
+      message.guild.channels.find(`name`, "pending").send(denyEmbed)
+    };
+    
+    // Accept
+    if (msg.split(" ")[0] === prefix + "accept"){
+      message.delete()
+      let args = msg.split(" ").slice(1)
+      let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+      if (!message.member.roles.has(Owner.id) && !message.member.roles.has(Staff.id)) return message.channel.send("You do not have access to this command")
+      if (!rUser) return message.channel.send('This user doesn\'t exist')
+      message.guild.channels.find(`name`, "general").send(`Welcome our newest member, ${rUser}!`)
+    };
+
     // Delete msgs
     if (msg.split(" ")[0] === prefix + "mdelete"){
         if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
@@ -103,6 +138,7 @@ bot.on('message', async message => {
         }else {return}
     };
 
+
     //Single Poll
     if (msg.startsWith("poll:")) {
       if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) { 
@@ -111,6 +147,7 @@ bot.on('message', async message => {
             let m3 = await message.react("🤷")
         } else {return};
       };
+
 
     //4poll
     if (msg.startsWith("4poll:")) {
@@ -122,17 +159,20 @@ bot.on('message', async message => {
         } else {return};
       };
 
+
     //get ping role
     if (msg === prefix + "pingrole"){
         message.member.addRole('501888773710282755');
         await message.reply('I have given you the ping role!')
     };
     
+
     //remove ping role
     if (msg === prefix + "rpingrole"){
         message.member.removeRole('501888773710282755');
         await message.reply('I have removed the ping role from you!')
     };
+
 
     //timed message
     //const generalchat = bot.channels.get("469490700845580298")
@@ -176,6 +216,7 @@ bot.on('message', async message => {
 
     };
 
+
     //member info
     if (msg.split(" ")[0] === prefix + "member") {
       //ex `member @Rinkky
@@ -197,6 +238,7 @@ bot.on('message', async message => {
           await message.channel.send(memberembed)
 
     };
+
 
     //role info
     if (msg.split(" ")[0] === prefix + "roleinfo") {
@@ -253,7 +295,7 @@ bot.on('message', async message => {
                  timestamp: new Date(),
                   footer: {
                   icon_url: "186487324517859328".avatarURL,
-                  text: "Any intentionally misleading reports will not be tolorated"
+                  text: `Any intentionally misleading reports \nwill not be tolorated`
                   }
                 }})
               })
@@ -312,6 +354,7 @@ bot.on('message', async message => {
           }
         };
     
+
     // Dice roll guess
     const roll =  Math.floor((Math.random() * 6) + 1);
     if(msg.split(" ")[0] === prefix + "diceroll"){
@@ -331,6 +374,7 @@ bot.on('message', async message => {
         }
        };
     
+
     // Work
     if(msg === prefix + "work"){
         if (workCooldown.has(sender.id)) {
@@ -347,6 +391,8 @@ bot.on('message', async message => {
             }, 600000);
         }
     };
+
+
     // Add money
     if(msg.split(" ")[0] === prefix + "addmoney"){
         if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
@@ -367,6 +413,7 @@ bot.on('message', async message => {
         }else {return}
     };
     
+
     // Remove money
     if(msg.split(" ")[0] === prefix + "removemoney"){
         if(sender.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
