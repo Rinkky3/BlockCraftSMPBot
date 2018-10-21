01412311fec3a0b5dcd52290d61153f7d2493c34
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2018-10-20 21:50:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -575,8 +575,8 @@ bot.on('message', async message => {
     } else if(msg === prefix + "mstop"){
         if(!message.member.voiceChannel) return message.channel.send("You aren't in a voice channel!")
         if(!serverQueue) return message.channel.send("Nothing is playing!")
-        queue.delete(message.guild.id)
-        serverQueue.voiceChannel.leave();
+	serverQueue.voiceChannel.leave();
+        queue.delete(message.guild.id)   
         return message.channel.send("Good bye!");
     }else if(msg === prefix + "skip"){
         if(!message.member.voiceChannel) return message.channel.send("You aren't in a voice channel!")
