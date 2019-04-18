eb05a5284f45dea51673f2491c75cca83d3721aa
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 09:00:45 -0400

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,4 +1,14 @@
 module.exports.run = async(bot, message, prefix) =>{
+  let sender = message.author; // The person who sent the message
+  let msg = message.content.toLowerCase();
+  let prefix = '`' // The text before commands
+  if (bot.user.id === message.author.id) { return }
+  let nick = sender.username
+  let Owner = message.guild.roles.find(r => r.name == "Owner")    
+  let Staff = message.guild.roles.find(r => r.name == "Staff")
+  let PlayerRole = message.guild.roles.find(r => r.name == "Player")
+  let muted = message.guild.roles.find(r => r.name == "Muted")
+  let pingRole = message.guild.roles.find(r => r.name == "Ping")
   let Owner = message.guild.roles.find(r => r.name == "Owner")
   if(message.author.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
     let m = await message.channel.send("Ping?");
