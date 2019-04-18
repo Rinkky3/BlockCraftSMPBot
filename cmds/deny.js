59db59f6c398f06267cc19ef8723509a6160919d
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 08:53:33 -0400

    Update deny.js

--- cmds/deny.js
+++ cmds/deny.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js")
 module.exports.run = async(bot, message, prefix) =>{
   let Owner = message.guild.roles.find(r => r.name == "Owner")    
   let Staff = message.guild.roles.find(r => r.name == "Staff")
