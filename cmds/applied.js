21216a94dcb14aeaaecc7ccb9b2d2b5e16b58109
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 09:04:34 -0400

    Update applied.js

--- cmds/applied.js
+++ cmds/applied.js
@@ -1,7 +1,18 @@
 const Discord = require("discord.js")
 module.exports.run = async(bot, message, prefix) =>{
-  let appchannel = message.guild.channels.find(c => c.name == "staff")
+  let sender = message.author; // The person who sent the message
+  let msg = message.content.toLowerCase();
+  let prefix = '`' // The text before commands
+  if (bot.user.id === message.author.id) { return }
+  let nick = sender.username
+  let Owner = message.guild.roles.find(r => r.name == "Owner")    
   let Staff = message.guild.roles.find(r => r.name == "Staff")
+  let PlayerRole = message.guild.roles.find(r => r.name == "Player")
+  let muted = message.guild.roles.find(r => r.name == "Muted")
+  let pingRole = message.guild.roles.find(r => r.name == "Ping")
+  let pending = message.guild.roles.find(r => r.name == "In-Progress")    
+  let args = msg.split(" ").slice(1)
+  let appchannel = message.guild.channels.find(c => c.name == "staff")
   let pending = message.guild.roles.find(r => r.name == "In-Progress")    
   if (!message.member.roles.has(pending.id)) return message.channel.send(sender + ", you are not in-progress!")
   if(bot.userData[message.author.id].appsNumber === 3) return message.channel.send(sender + ', you have exceeded your maximum number of applications, if this is a mistake, please contact <@186487324517859328> or <@353782817777385472>')
