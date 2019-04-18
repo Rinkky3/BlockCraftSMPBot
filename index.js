68fccf75b36ca2caf96e05e5736a22aceb674383
Rinkky <vurvur1@hotmail.gr>
2019-04-18 15:19:10 +0300

    removed index.js ping

--- index.js
+++ index.js
@@ -143,15 +143,6 @@ bot.on('message', async message => {
     }
 
 
-    // Ping / Pong command
-    if (msg === prefix + 'ping') {
-      if(message.author.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
-        let m = await message.channel.send("Ping?");
-        m.edit(`Pong. Latency: ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
-      } else {return}
-    };
-    
-
     //Single Poll
     if (msg.startsWith("poll:")) {
       if(message.author.id === "186487324517859328" || message.member.roles.has(Owner.id)) { 
