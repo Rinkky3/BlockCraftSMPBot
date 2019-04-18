246b9e837c9a409967b9c9ef0a602e38eabd0623
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 09:10:17 -0400

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,7 +1,18 @@
 module.exports.run = async(bot, message, prefix) =>{
 	console.log('HELP INITIATED!')
+	let sender = message.author; // The person who sent the message
+	let msg = message.content.toLowerCase();
+	let prefix = '`' // The text before commands
+	if (bot.user.id === message.author.id) { return }
+	let nick = sender.username
+	let Owner = message.guild.roles.find(r => r.name == "Owner")    
+	let Staff = message.guild.roles.find(r => r.name == "Staff")
+	let PlayerRole = message.guild.roles.find(r => r.name == "Player")
+	let muted = message.guild.roles.find(r => r.name == "Muted")
+	let pingRole = message.guild.roles.find(r => r.name == "Ping")
+	let pending = message.guild.roles.find(r => r.name == "In-Progress")    
+	let args = msg.split(" ").slice(1)
 	let args = message.content.toLowerCase().split(" ").slice(1);
-	console.log(args[0])
 	const Discord = require("discord.js")
 	if(!args[0]){
 	  const commands = ["pingrole", "leaderboard", "rpingrole", "botinfo", "serverinfo", "roleinfo", "member", "report", "play", "skip", "volume", "np", "queue"]
