4800e7e26624c24b886cccc10528835d9a3d9fb0
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 09:04:02 -0400

    Update accept.js

--- cmds/accept.js
+++ cmds/accept.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js")
 module.exports.run = async(bot, message, prefix) =>{
-  let Owner = message.guild.roles.find(g => g.name == "Owner")
+  let sender = message.author; // The person who sent the message
+  let msg = message.content.toLowerCase();
+  let prefix = '`' // The text before commands
+  if (bot.user.id === message.author.id) { return }
+  let nick = sender.username
+  let Owner = message.guild.roles.find(r => r.name == "Owner")    
+  let Staff = message.guild.roles.find(r => r.name == "Staff")
   let PlayerRole = message.guild.roles.find(r => r.name == "Player")
+  let muted = message.guild.roles.find(r => r.name == "Muted")
+  let pingRole = message.guild.roles.find(r => r.name == "Ping")
   let pending = message.guild.roles.find(r => r.name == "In-Progress")    
   let args = msg.split(" ").slice(1)
   let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
