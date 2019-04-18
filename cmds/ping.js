296be9683cbede2269b0c3de3a7e26f6a6950005
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 09:19:38 -0400

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -8,7 +8,6 @@ module.exports.run = async(bot, message, prefix) =>{
   let PlayerRole = message.guild.roles.find(r => r.name == "Player")
   let muted = message.guild.roles.find(r => r.name == "Muted")
   let pingRole = message.guild.roles.find(r => r.name == "Ping")
-  let Owner = message.guild.roles.find(r => r.name == "Owner")
   if(message.author.id === "186487324517859328" || message.member.roles.has(Owner.id)) {
     let m = await message.channel.send("Ping?");
     m.edit(`Pong. Latency: ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
