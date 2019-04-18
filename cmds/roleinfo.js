e49bdf62a2a2346975fc28deccfdb6eef98f5c74
steven4547466 <23726534+steven4547466@users.noreply.github.com>
2019-04-18 08:54:46 -0400

    Update roleinfo.js

--- cmds/roleinfo.js
+++ cmds/roleinfo.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js")
 module.exports.run = async(bot, message, prefix) =>{
   let rRole = message.mentions.roles.first()
   if(!rRole) return message.reply("Who dat role? I cant find it.")
