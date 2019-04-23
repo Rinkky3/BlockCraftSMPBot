21f5cf81ddb35a0d7eb9b730579b5efd92f2ad95
Rinkky <vurvur1@hotmail.gr>
2019-04-23 17:02:59 +0300

    more changes on accepted

--- cmds/accept.js
+++ cmds/accept.js
@@ -6,6 +6,10 @@ module.exports.run = async(bot, message, prefix) =>{
   let Staff = message.guild.roles.find(r => r.name == "Staff")
   let AcceptedRole = message.guild.roles.find(r => r.name == "Accepted")
   let pending = message.guild.roles.find(r => r.name == "In-Progress")    
+  const channelinfo = member.guild.channels.find(c => c.name == "info");
+  const channelannounce = member.guild.channels.find(c => c.name == "accouncements");
+  const channelfaq = member.guild.channels.find(c => c.name == "faq");
+  const channelideas = member.guild.channels.find(c => c.name == "ideas");
   let args = msg.split(" ").slice(1)
   let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
   message.delete(1000)
@@ -13,6 +17,6 @@ module.exports.run = async(bot, message, prefix) =>{
   if (!rUser) return message.channel.send('This user doesn\'t exist')
   rUser.addRole(AcceptedRole.id);
   rUser.removeRole(pending.id);
-  message.guild.channels.find(c => c.name == "general").send(`Welcome our newest member, ${rUser}! \n\n You will be whitelisted 1 week before the reset, and will be able to roam around in spectator mode! Make sure to check the #info, #faq and #announcements channels, for any updates, throw your ideas on #ideas, if you have any questions tag Rinkky and have a good one!`)
+  message.guild.channels.find(c => c.name == "general").send(`Welcome our newest member, ${rUser}! \n\n You will be whitelisted 1 week before the reset, and will be able to roam around in spectator mode! Make sure to check the ${channelinfo}, ${channelfaq} and ${channelannounce} channels, for any updates, throw your ideas on ${channelideas}, if you have any questions tag Rinkky and have a good one!`)
   
 }
